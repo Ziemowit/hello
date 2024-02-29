65504deea95f3749d510394fe673c050ac9857a9
Ziemowit Stolarczyk <ziemowits@wayfarerpoints.com>
2024-02-29 14:30:13 +0100

    Init

---
+++ src/main/java/org/example/hello/HelloController.java
@@ -0,0 +1,16 @@
+package org.example.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  private final String MSG = "Hello version1";
+
+  @GetMapping("/hello")
+  public String hello() {
+    return MSG;
+  }
+
+}
